35ac9978f47629cf40b9ea0f22435bc2b446fd26
sanjay <sanjaypython22@gmail.com>
2025-12-17 22:56:46 +0530

    p

--- exclusive/settings.py
+++ exclusive/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'news.context_processors.category_context',
+                'news.context_processors.breaking_news',
             ],
         },
     },
--- news/context_processors.py
+++ news/context_processors.py
@@ -1,8 +1,16 @@
 # news/context_processors.py
 
 from category.models import Category
+from .models import News
+
 
 def category_context(request):
     return {
         'category': Category.objects.all().order_by('-id')
+    }
+
+
+def breaking_news(request):
+    return {
+        'breaking_news': News.objects.filter().order_by('-created_at')
     }
