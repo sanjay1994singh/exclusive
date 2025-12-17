788d88e480931ee4b23626f4501bd394d8b2814b
sanjay <sanjaypython22@gmail.com>
2025-12-17 22:26:14 +0530

    p

--- homepage/urls.py
+++ homepage/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.homepage, name='homepage'),
+    path('news-detail/<int:id>/', views.news_detail, name='news_detail'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render
 
+from news.models import News
+import random
+
+from category.models import Category
+
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+def homepage(request):
+    categories = Category.objects.all()
+    news = News.objects.all().order_by('-id')
+    context = {
+        'news': news,
+        'categories': categories,
+    }
+
+    return render(request, 'index.html', context)
+
+
+def news_detail(request, id):
+    news = News.objects.get(id=id)
+    count = news.count
+    number = random.randint(1, 5)
+    total_count = int(number + count)
+    news.count = total_count
+    news.save()
+    try:
+        absolute_image_url = request.build_absolute_uri(news.featured_image.url)
+    except:
+        absolute_image_url = ''
+    category = Category.objects.all().order_by('-id')
+    context = {
+        'news': news,
+        'absolute_image_url': absolute_image_url,
+        'category': category,
+    }
+    return render(request, 'detail.html', context)
 
 
 def about(request):
