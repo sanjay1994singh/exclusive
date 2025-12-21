c34cb32160b89bcf01e4ea6dd7400cef46a982b6
sanjay <sanjaypython22@gmail.com>
2025-12-21 22:13:02 +0530

    p

--- news/context_processors.py
+++ news/context_processors.py
@@ -2,13 +2,19 @@
 
 from category.models import Category
 from .models import News
+from django.db.models import Max
 
+# def category_context(request):
+#     return {
+#         'category': Category.objects.all().order_by('-id')
+#     }
 
 def category_context(request):
     return {
-        'category': Category.objects.all().order_by('-id')
+        'categories': Category.objects.annotate(
+            latest_news=Max('news__created_at')
+        ).order_by('-latest_news')
     }
-
 
 def breaking_news(request):
     return {
